f8c3e6dddb95304d939708c0f00ed7f46dee111b
CharlieS1103 <someonelikeswindows@gmail.com>
2022-10-03 21:25:45 -0400

    Preload images

--- src/Components/Sections/Projects.js
+++ src/Components/Sections/Projects.js
@@ -1,39 +1,46 @@
 //@ts-check
 
 import "../../styles/Projects.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import React from "react";
+
+const projects = [
+  {
+    name: "Chaos Chess",
+    description:
+      "A chess game with a twist. Many different custom gamemodes are available.",
+    image: "https://i.imgur.com/Sq17jFd.png",
+    link: "https://github.com/CharlieS1103/ChaosChess",
+    statement:
+      "I made this project as a way to learn how to integrate a full website with both a backend and a frontend. It was also inspired by my interest in chess, and served as a playground for my own original gamemodes. It obviously has no practical use, and the UI has a lot of room to grow, but it was a fun project to make.",
+  },
+  {
+    name: "Spicetify Marketplace",
+    description:
+      "A marketplace for Spicetify users to install and manage themes and extensions using inline-linking",
+    image: "https://i.imgur.com/Ev9dXvx.png",
+    link: "https://github.com/Spicetify/spicetify-marketplace",
+    statement:
+      "This project was the result of a need for a marketplace within Spicetify. \nBeforehand, users would have to download themes and/or extensions and manually edit the config file, which is a pain. \nIt serves as a user interface for installation, in addition, it has able to work around the limitations of not being able to interact with the Filesystem, hence the use of inline-linking to serve extensions and themes.",
+  },
+  {
+    name: "Hide Streaming Controls",
+    description:
+      "A Chrome extension that hides the streaming controls on various streaming platforms.",
+    image: "https://i.imgur.com/eCJC7Dl.png",
+    link: "https://github.com/CharlieS1103/hide-streaming-controls",
+    statement:
+      "Although this project was not the most complicated to make, it serves a huge practical purpose for me, as I dislike having the streaming controls blind me whenever I move my mouse.",
+  },
+];
 function Projects() {
   const [count, setCount] = useState(0);
-  const projects = [
-    {
-      name: "Chaos Chess",
-      description:
-        "A chess game with a twist. Many different custom gamemodes are available.",
-      image: "https://i.imgur.com/Sq17jFd.png",
-      link: "https://github.com/CharlieS1103/ChaosChess",
-      statement:
-        "I made this project as a way to learn how to integrate a full website with both a backend and a frontend. It was also inspired by my interest in chess, and served as a playground for my own original gamemodes. It obviously has no practical use, and the UI has a lot of room to grow, but it was a fun project to make.",
-    },
-    {
-      name: "Spicetify Marketplace",
-      description:
-        "A marketplace for Spicetify users to install and manage themes and extensions using inline-linking",
-      image: "https://i.imgur.com/Ev9dXvx.png",
-      link: "https://github.com/Spicetify/spicetify-marketplace",
-      statement:
-        "This project was the result of a need for a marketplace within Spicetify. \nBeforehand, users would have to download themes and/or extensions and manually edit the config file, which is a pain. \nIt serves as a user interface for installation, in addition, it has able to work around the limitations of not being able to interact with the Filesystem, hence the use of inline-linking to serve extensions and themes.",
-    },
-    {
-      name: "Hide Streaming Controls",
-      description:
-        "A Chrome extension that hides the streaming controls on various streaming platforms.",
-      image: "https://i.imgur.com/eCJC7Dl.png",
-      link: "https://github.com/CharlieS1103/hide-streaming-controls",
-      statement:
-        "Although this project was not the most complicated to make, it serves a huge practical purpose for me, as I dislike having the streaming controls blind me whenever I move my mouse.",
-    },
-  ];
+  // When page is rendered (componentMount), load in the images
+  useEffect(() => {
+    projects.forEach((project) => {
+      new Image().src = project.image;
+    });
+  }, []);
   return (
     <div
       className="projects-container"
@@ -77,6 +84,8 @@ function Projects() {
     </div>
   );
 }
+
+
 function increment(projects, setCount, count) {
   // Set the count as long as it is less than the length of the projects array
 
